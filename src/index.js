2afafcf84fd5eba460528662fb0465ee5da1331b
Justin <justinms@outlook.com>
2020-07-05 13:30:07 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,124 +1,108 @@
-let addToy = false;
-
-document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
-  const toyForm = document.getElementById("add-toy-form");
-  addToys();
-
-  function addToyToPage(id, name, image, likes) {
-    const toyCollection = document.getElementById("toy-collection");
-
-    const div = document.createElement('div');
-    div.setAttribute("id", name)
-    const h2 = document.createElement('h2');
-    h2.innerText = name;
-    div.appendChild(h2);
-
-    const img = document.createElement('img');
-    img.setAttribute("src", image);
-    img.setAttribute("class", "toy-avatar");
-    div.appendChild(img);
-
-    const p = document.createElement('p');
-    p.innerText = `${likes} Likes`;
-    div.appendChild(p);
-
-    const bttn = document.createElement('button');
-    bttn.setAttribute("class", "like-btn");
-    bttn.setAttribute("value", id);
-    bttn.innerText = "Like <3";
-
-    bttn.addEventListener("click", function(event){
-      increaseLikes(event.srcElement.value);
-    });
-
-    div.appendChild(bttn);
-    toyCollection.append(div)
-
-  }
-
-  function increaseLikes(toy){
-
-    fetch(`http://localhost:3000/toys/${toy}`).then(function(response){
+// Returns data from DB in JSON
+function fetchToys(){
+  return fetch('http://localhost:3000/toys').then(function(response){ 
       return response.json();
-    }).then(function(toyObject){
-      let toyLikes = toyObject.likes += 1;
+  })
+}
 
-      let formData = {
-        likes: toyLikes
-      }
+// Adds toys from fetchToys() onto web page
+function renderToys(toy){
+  const toyCollection = document.querySelector("#toy-collection");
 
-      let configObj = {
-        method: "PATCH",
+  const div = document.createElement('div');
+  div.setAttribute("id", toy.name)
+
+  const h2 = document.createElement('h2');
+
+  const img = document.createElement('img');
+  img.setAttribute("src", toy.image);
+  img.setAttribute("class", "toy-avatar");
+
+  const p = document.createElement('p');
+  p.innerText = `${toy.likes} Likes`;
+
+  const btn = document.createElement('button');
+  btn.setAttribute("class", "like-btn");
+  btn.setAttribute("id", toy.id);
+  btn.innerText = "Like <3";
+
+  btn.addEventListener("click", function(event){
+    addLike(event)
+  });
+
+  div.append(h2, img, p, btn);
+  toyCollection.append(div)
+}
+
+// Iterate over toys, add each toy to web page
+fetchToys().then(function(toys){
+  for(const toy of toys){
+    renderToys(toy);
+  }
+})
+
+let addToy = false;
+const addBtn = document.getElementById('new-toy-btn');
+const toyFormContainer = document.querySelector('.container');
+
+// Toggles between showing and hiding toy form
+addBtn.addEventListener("click", () => {
+  addToy = !addToy;
+  if (addToy) {
+    toyFormContainer.style.display = "block";
+     
+    toyFormContainer.addEventListener('submit', function(event) {
+      postToy(event.target);
+      event.preventDefault();
+    })
+  } 
+  else {
+    toyFormContainer.style.display = "none";
+  }
+});
+
+// Adds toy from toy form
+function postToy(toy) {
+  let configObj = {
+        method: "POST",
         headers: {
           "Content-Type": "application/json",
           "Accept": "application/json"
         },
-        body: JSON.stringify(formData) 
+        body: JSON.stringify({
+          "name": toy.name.value,
+          "image": toy.image.value,
+          "likes": 0
+        })
       }
 
-      fetch(`http://localhost:3000/toys/${toy}`, configObj).then(function(response){
-        return response.json();
-      }).then(function(toyObject){
-        let toyLikes = document.querySelector(`div#${toyObject.name} p`)
-        toyLikes.innerText = `${toyObject.likes} Likes`
-      })
+  fetch('http://localhost:3000/toys', configObj).then(function(response){
+    return response.json();
+
+  }).then(function(toyObject){
+    renderToys(toyObject);
+  })
+}
+
+// Increase toy likes
+function addLike(event){
+  let increasedLikes = parseInt(event.target.previousElementSibling.innerText) + 1;
+  
+  let configObj = {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      "likes": increasedLikes
     })
-
-
-
   }
 
- function addToys(){
-  fetch('http://localhost:3000/toys').then(function(response){
+  fetch(`http://localhost:3000/toys/${event.target.id}`, configObj).then(function(response){
     return response.json();
-  }).then(function(json) {
-    for (const toy of json){
-      addToyToPage(toy.id, toy.name, toy.image, toy.likes)
-    }
+
+  }).then(function(toyObject){
+    event.target.previousElementSibling.innerText = `${increasedLikes} Likes`;
   })
- }
-  
-  addBtn.addEventListener("click", () => {
-    // hIde & seek with the form
-    addToy = !addToy;
-    if (addToy) {
-      toyFormContainer.style.display = "block";
-    } else {
-      toyFormContainer.style.display = "none";
-    }
-  })
-
-  toyForm.addEventListener("submit", function(event){
-    const toyName = document.getElementById("name").value;
-    const toyImage = document.getElementById("image").value;
-
-    let formData = {
-      name: toyName,
-      image: toyImage,
-      likes: 0
-    }
-
-    let configObj = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-      },
-      body: JSON.stringify(formData),
-    }
-
-    fetch("http://localhost:3000/toys", configObj).then(function(response){
-      return response.json();
-
-    }).then(function(toyObject){
-      addToyToPage(toyObject.name, toyObject.image, toyObject.likes);
-
-    }).catch(function(error){
-      console.log(error.message);
-    });
-
-    event.preventDefault();
-  });
-});
+}
